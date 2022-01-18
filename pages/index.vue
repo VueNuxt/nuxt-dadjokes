99cb87b8d00628036289f4fa973ccbd781d6a520
Johan Melin <soldierheart@hotmail.com>
2022-01-18 10:04:44 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Corny dad jokes</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to DadJokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
